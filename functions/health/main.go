fb2368b18e3d84e0dafec8f893b073920fdda0fe
Magnun A V F <magnunavf@gmail.com>
2023-06-25 12:58:04 -0300

    Add env vars support

--- functions/health/main.go
+++ functions/health/main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func Health(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	response := events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "OK",
-	}
+type HealthResponse struct {
+	Status string `json:"status"`
+	Env    string `json:"env"`
+}
 
-	return response, nil
+func Health(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	response := HealthResponse{Status: "OK", Env: os.Getenv("ENV")}
+	responseBody, _ := json.Marshal(response)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(responseBody),
+	}, nil
 }
 
 func main() {
